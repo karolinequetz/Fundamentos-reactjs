e7d70f19e284a219501bcff44f8e239d8f22ac2c
karolsouzaq@gmail.com <karolsouzaq@gmail.com>
2022-07-08 12:32:54 -0300

    Criado componente para avatar.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import styles from './Sidebar.module.css';
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
 export function Sidebar() {
 	return (
 		<aside className={styles.sidebar}>
@@ -8,8 +9,7 @@ export function Sidebar() {
 				src="https://images.unsplash.com/photo-1570458436416-b8fcccfe883f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
 			/>
 			<div className={styles.profile}>
-				<img className={styles.avatar} src="https://github.com/karolinequetz.png" />
-
+				<Avatar src="https://github.com/karolinequetz.png" />
 				<strong>Karoline Quetz</strong>
 				<span>Web Developer</span>
 			</div>
