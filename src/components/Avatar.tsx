a35a6e7d94e936d2c9017dd84ed154838c06c880
karolsouzaq@gmail.com <karolsouzaq@gmail.com>
2022-07-13 23:09:44 -0300

    Adaptada aplicação para usar TypeScript.

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,10 +1,10 @@
+import { ImgHTMLAttributes } from 'react';
 import styles from './Avatar.module.css';
 
-interface AvatarProps {
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
 	hasBorder?: boolean;
-	src: string;
-	alt?: string;
 }
-export function Avatar({ hasBorder = true, src, alt }: AvatarProps) {
-	return <img className={hasBorder ? styles.avatarWithBorder : styles.avatar} src={src} alt={alt} />;
+
+export function Avatar({ hasBorder = true, ...props }: AvatarProps) {
+	return <img className={hasBorder ? styles.avatarWithBorder : styles.avatar} {...props} />;
 }
