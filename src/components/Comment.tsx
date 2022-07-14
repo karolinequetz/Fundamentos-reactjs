4b3dce44800f927b68c2b2bf49db6b8ef23be1d4
karolsouzaq@gmail.com <karolsouzaq@gmail.com>
2022-07-13 22:36:04 -0300

    Adaptada aplicação para usar TypeScript.

---
+++ src/components/Comment.tsx
@@ -0,0 +1,53 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { useState } from 'react';
+import { Avatar } from './Avatar';
+import styles from './Comment.module.css';
+
+interface CommentProps {
+	content: string;
+	onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+	const [ likeCount, setLikeCount ] = useState(0);
+
+	function handleDeleteComment() {
+		onDeleteComment(content);
+	}
+
+	function handleLikeComment() {
+		setLikeCount((state) => {
+			return state + 1;
+		});
+	}
+
+	return (
+		<div className={styles.comment}>
+			<Avatar hasBorder={false} src="https://github.com/karolinequetz.png" alt="" />
+
+			<div className={styles.commentBox}>
+				<div className={styles.commentContent}>
+					<header>
+						<div className={styles.authorAndTime}>
+							<strong>Karoline Quetz</strong>
+							<time title="07 de Julho às 11:18h" dateTime="2022-07-07">
+								Cerca de 1h atrás
+							</time>
+						</div>
+						<button onClick={handleDeleteComment} title="Deletar comentário">
+							<Trash size={24} />
+						</button>
+					</header>
+					<p>{content}</p>
+				</div>
+				<footer>
+					<button onClick={handleLikeComment}>
+						<ThumbsUp />
+						Aplaudir
+						<span>{likeCount}</span>
+					</button>
+				</footer>
+			</div>
+		</div>
+	);
+}
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,24 @@
+import styles from './Sidebar.module.css';
+import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
+export function Sidebar() {
+	return (
+		<aside className={styles.sidebar}>
+			<img
+				className={styles.cover}
+				src="https://images.unsplash.com/photo-1570458436416-b8fcccfe883f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+			/>
+			<div className={styles.profile}>
+				<Avatar src="https://github.com/karolinequetz.png" />
+				<strong>Karoline Quetz</strong>
+				<span>Web Developer</span>
+			</div>
+			<footer>
+				<a href="#">
+					<PencilLine size={20} />
+					Editar seu perfil
+				</a>
+			</footer>
+		</aside>
+	);
+}
